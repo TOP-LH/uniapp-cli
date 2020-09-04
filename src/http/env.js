81e237a6e4e46f0b0d61eb5828ff23c45e1f9d1a
lihao <49888630+HaoliHope@users.noreply.github.com>
2020-09-04 14:56:55 +0800

    Update env.js

--- src/http/env.js
+++ src/http/env.js
@@ -6,11 +6,11 @@ let env = {}
 let pro = process.env.NODE_ENV === 'production'
 let dev = process.env.NODE_ENV === 'development'
 
-if (dev) env.API_HOST = 'http://10.88.45.249:8769/' // 开发环境api
-if (pro) env.API_HOST = 'https://takeout.wison.com/' // 生产环境api
+if (dev) env.API_HOST = 'http://localhost:8769/' // 开发环境api
+if (pro) env.API_HOST = 'https://aaa.bbb.com/' // 生产环境api
 
-env.LOCAL_HOST = 'http://localhost:8080/WisonWebApp/Wison.WebApp.Forms/#/portal/index'
-env.API_PREFIX = '/WisonWebApp/Wison.WebApi.QRCode/'
+env.LOCAL_HOST = 'http://localhost:8080/'
+env.API_PREFIX = '/wuhu/qifei/'
 env.API_PREFIX_VERSION_V1_1 = '/api/v1.1'
 
 export default env
